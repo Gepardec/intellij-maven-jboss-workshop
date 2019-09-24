2774fd9426a28742be68b11b4dc4e00638706aae
fhochleitner <felix.hochleitner@gepardec.com>
2019-09-24 11:52:37 +0200

    Compiling including tests.

--- intellij-maven-jboss-helloworld-service/src/main/java/at/gepardec/intellij/maven/jboss/service/HelloService.java
+++ intellij-maven-jboss-helloworld-service/src/main/java/at/gepardec/intellij/maven/jboss/service/HelloService.java
@@ -1,5 +1,8 @@
 package at.gepardec.intellij.maven.jboss.service;
 
+import javax.ejb.Stateless;
+
+@Stateless
 public class HelloService {
     public String createHelloMessage(String name) {
         return "Hello " + name + "!";
