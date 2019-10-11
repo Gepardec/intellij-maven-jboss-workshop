ebc3c3e908f20c8db5aa3f27d47ca0cfcf20e7ac
jriegler <johannes.riegler@gepardec.com>
2019-10-11 09:41:35 +0200

    "Clean up"

--- intellij-maven-jboss-helloworld-service/src/main/java/at/gepardec/intellij/maven/jboss/service/HelloService.java
+++ intellij-maven-jboss-helloworld-service/src/main/java/at/gepardec/intellij/maven/jboss/service/HelloService.java
@@ -1,5 +1,8 @@
 package at.gepardec.intellij.maven.jboss.service;
 
+import javax.enterprise.context.Dependent;
+
+@Dependent
 public class HelloService {
     public String createHelloMessage(String name) {
         return "Hello " + name + "!";
